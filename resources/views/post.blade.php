88a7f94797c24280ff1374827033cdda922c8077
Kchaou chedly <82588422+chedlykch@users.noreply.github.com>
2022-07-13 12:44:38 +0200

    Commit
     lesson 7
    CHANGES
    ROUTES + LINKS

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<title> My Cour </title>
+
+<link rel = "stylesheet" href="/app.css">
+
+<body>
+    <article>
+
+
+    <h1 ><a href="/post"> Hello </a></h1>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, fuga blanditiis itaque architecto quibusdam veritatis reprehenderit amet eligendi corrupti officiis delectus quisquam ipsum earum officia vitae fugiat sunt enim deserunt.
+</P>
+
+
+
+
+
+    </article><a href="/">Go Back</a>
+  
+</body> 
--- resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<title> My Cour </title>
-
-<link rel = "stylesheet" href="/app.css">
-<script src="/app.js"></script>
-<body>
-    <h1>Hello </h1>
-</body>
